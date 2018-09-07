be622905e21445d224ab50d565672b3bd67cc775
Xiaoyu LI <toysoldier2012@gmail.com>
2018-09-07 23:32:03 +0200

    Login TCP exercise

--- net/tcp/login/Client.java
+++ net/tcp/login/Client.java
@@ -1,5 +1,49 @@
 package net.tcp.login;
 
-public class Client {
+import java.net.*;
+import java.io.*;
 
+public class Client{
+	/*
+	 * Normally, it should be encapsulated in two part(method): 
+	 * one for type in username by user
+	 * another one for send the information to server
+	 * and i didn't handle the exceptions
+	 */
+	public void Init() throws UnknownHostException, IOException  {
+		Socket s = new Socket("192.168.0.27",10040);
+		PrintStream socketPS = new PrintStream(s.getOutputStream(), true);
+		DataInputStream socketDIS = new DataInputStream(s.getInputStream());
+		BufferedReader systemBR = new BufferedReader(new InputStreamReader(System.in));
+		String username;
+		Boolean flag;
+		
+		for(int i = 0; i < 3; i++){			
+			System.out.println("Enter your username: ");
+			username = systemBR.readLine();
+			while(username.isEmpty()) {
+				System.out.println("You must have a name!");
+				username = systemBR.readLine();
+			}
+			socketPS.println(username);
+			flag = socketDIS.readBoolean();
+
+			if(!flag) {
+				if(i < 2)
+					System.out.println("try again!");
+				else System.out.println("You shall not pass!");
+			}
+			else {
+				System.out.println("Welcome!");
+				break ;
+			}
+		}
+		
+		s.close();
+		systemBR.close();
+	}
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		new Client().Init();
+	}
 }
--- net/tcp/login/Server.java
+++ net/tcp/login/Server.java
@@ -1,5 +1,95 @@
 package net.tcp.login;
 
-public class Server {
+import java.io.*;
+import java.util.*;
+import java.net.*;
 
+class HandleUsernameThread implements Runnable {
+	private Socket s;
+	private HashSet<String> usernameList;
+	HandleUsernameThread(Socket s, HashSet<String> usernameList){
+		this.s = s;
+		this.usernameList = usernameList;
+	}
+
+	public void run() {
+		try {
+			BufferedReader socketBR = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			DataOutputStream socketDOS = new DataOutputStream(s.getOutputStream());
+			
+			String username;
+			for(int i = 0; i < 3; i++) {
+				username = socketBR.readLine();
+				if(usernameList.contains(username)) {
+					socketDOS.writeBoolean(true);
+					System.out.println(s.getInetAddress().getHostAddress()+" "+ username +" log in");
+					return ;
+				} else {
+					socketDOS.writeBoolean(false);
+					System.out.println(s.getInetAddress().getHostAddress()+" "+ username +" try to login");	
+					System.out.println("Times:"+ i);
+				}
+			}
+			System.out.println(s.getInetAddress().getHostAddress()+" shall not pass!");
+			
+			s.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+class ShutdownServerThread implements Runnable {
+	private ServerSocket ss;
+	private BufferedReader systemBR;
+	private String shutDownServer = null;
+	
+	ShutdownServerThread(ServerSocket ss){
+		this.ss = ss;
+	}
+	
+	public void run() {
+		systemBR = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			shutDownServer = systemBR.readLine();
+			systemBR.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		if(shutDownServer.equals("SHUTDOWNSERVER"))
+			try {
+				
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+}
+
+public class Server {
+	public void Init() throws IOException {
+		BufferedReader fileBR = new BufferedReader(new FileReader("usernameList.txt"));
+		String username;
+		HashSet<String> usernameList = new HashSet<>();
+		while((username = fileBR.readLine()) != null)
+			usernameList.add(username);
+		fileBR.close();
+		
+		ServerSocket ss = new ServerSocket(10040);
+		
+		new Thread(new ShutdownServerThread(ss)).start();
+		while(true) {
+			Socket s = ss.accept();
+			new Thread(new HandleUsernameThread(s, usernameList)).start();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new Server().Init();
+	}
 }
